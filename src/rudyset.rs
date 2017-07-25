3b326372ca1124218a3db9557e4691a618c761a2
Aaron DeVore <aaron.devore@gmail.com>
2017-07-25 03:41:53 -0700

    rudyset: Add test cases

--- src/rudyset.rs
+++ src/rudyset.rs
@@ -82,3 +82,78 @@ impl<K: Key + PartialEq> PartialEq for RudySet<K> {
 impl<K: Key + Eq> Eq for RudySet<K> {}
 
  */
+
+#[cfg(test)]
+mod test {
+    use super::*;
+    #[test]
+    fn test_insert_contains_1() {
+        let mut set = RudySet::<usize>::new();
+        set.insert(4usize);
+        assert_eq!(set.contains(4), true);
+        assert_eq!(set.contains(10), false);
+    }
+
+    #[test]
+    fn test_insert_contains_2() {
+        let mut set = RudySet::<usize>::new();
+        set.insert(0);
+        set.insert(1);
+        assert_eq!(set.contains(0), true);
+        assert_eq!(set.contains(1), true);
+        assert_eq!(set.contains(2), false);
+    }
+
+    #[test]
+    fn test_contains_key() {
+        let mut set = RudySet::<u32>::new();
+        assert_eq!(set.contains(0), false);
+        assert_eq!(set.contains(1), false);
+
+        assert_eq!(set.insert(0), false);
+        assert_eq!(set.contains(0), true);
+        assert_eq!(set.contains(1), false);
+
+        assert_eq!(set.remove(0), true);
+        assert_eq!(set.contains(0), false);
+        assert_eq!(set.contains(1), false);
+    }
+
+    #[test]
+    fn test_len() {
+        let mut set = RudySet::<u32>::new();
+        assert_eq!(set.len(), 0);
+        assert_eq!(set.is_empty(), true);
+
+        assert_eq!(set.insert(0), false);
+        assert_eq!(set.len(), 1);
+        assert_eq!(set.is_empty(), false);
+
+        assert_eq!(set.insert(1), false);
+        assert_eq!(set.len(), 2);
+        assert_eq!(set.is_empty(), false);
+
+        assert_eq!(set.insert(0), true);
+        assert_eq!(set.len(), 2);
+        assert_eq!(set.is_empty(), false);
+
+        assert_eq!(set.remove(0), true);
+        assert_eq!(set.len(), 1);
+        assert_eq!(set.is_empty(), false);
+
+        assert_eq!(set.remove(1), true);
+        assert_eq!(set.len(), 0);
+        assert_eq!(set.is_empty(), true);
+    }
+
+    #[test]
+    fn test_clear() {
+        let mut set = RudySet::<u32>::new();
+        assert_eq!(set.insert(0), false);
+        assert_eq!(set.len(), 1);
+
+        set.clear();
+        assert_eq!(set.len(), 0);
+        assert_eq!(set.is_empty(), true);
+    }
+}
